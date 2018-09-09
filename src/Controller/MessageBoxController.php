3363d744bb7b72f487bd0a4669c96851ed98d3b0
Sebastian Blum <github.com@sblum.de>
2018-09-09 12:19:14 +0200

    fix typo in route prefix

--- src/Controller/MessageBoxController.php
+++ src/Controller/MessageBoxController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/pi", methods={"POST"})
+ * @Route("/api", methods={"POST"})
  */
 class MessageBoxController extends Controller
 {
